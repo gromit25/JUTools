42e5733046539f9fc31912692a1820ec0539924b
gromit25 <gromit25@hanmail.net>
2023-02-26 16:47:36 +0900

    BytesUtil 클래스의 split 메소드에 대한 단위테스트 코드 작성

---
+++ src/test/java/com/jutools/BytesUtilTest.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package com.jutools;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+/**
+ * BytesUtil 클래스에 대한 단위 테스트 클래스
+ * 
+ * @author jmsohn
+ */
+public class BytesUtilTest {
+
+	@Test
+	public void testSplit1() throws Exception {
+		
+		String test1 = "123|||6789";
+	       
+		ArrayList<byte[]> splitedList = BytesUtil.split(test1.getBytes(), "|||".getBytes());
+	       
+		assertTrue(splitedList.size() == 2);
+		assertTrue(new String(splitedList.get(0)).equals("123"));
+		assertTrue(new String(splitedList.get(1)).equals("6789"));
+	}
+	
+	@Test
+	public void testSplit2() throws Exception {
+		
+		String test1 = "123||45|||6789";
+	       
+		ArrayList<byte[]> splitedList = BytesUtil.split(test1.getBytes(), "|||".getBytes());
+	       
+		assertTrue(splitedList.size() == 2);
+		assertTrue(new String(splitedList.get(0)).equals("123||45"));
+		assertTrue(new String(splitedList.get(1)).equals("6789"));
+	}
+	
+	@Test
+	public void testSplit3() throws Exception {
+		
+		String test1 = "123||||||6789";
+	       
+		ArrayList<byte[]> splitedList = BytesUtil.split(test1.getBytes(), "|||".getBytes());
+	       
+		assertTrue(splitedList.size() == 2);
+		assertTrue(new String(splitedList.get(0)).equals("123"));
+		assertTrue(new String(splitedList.get(1)).equals("6789"));
+	}
+	
+	@Test
+	public void testSplit4() throws Exception {
+		
+		String test1 = "123|||||||6789";
+	       
+		ArrayList<byte[]> splitedList = BytesUtil.split(test1.getBytes(), "|||".getBytes());
+	       
+		assertTrue(splitedList.size() == 2);
+		assertTrue(new String(splitedList.get(0)).equals("123"));
+		assertTrue(new String(splitedList.get(1)).equals("|6789"));
+	}
+
+	@Test
+	public void testSplit5() throws Exception {
+		
+		String test1 = "123|||6789|";
+	       
+		ArrayList<byte[]> splitedList = BytesUtil.split(test1.getBytes(), "|||".getBytes());
+	       
+		assertTrue(splitedList.size() == 2);
+		assertTrue(new String(splitedList.get(0)).equals("123"));
+		assertTrue(new String(splitedList.get(1)).equals("6789|"));
+	}
+	
+	@Test
+	public void testSplit6() throws Exception {
+		
+		String test1 = "123|||6789|||";
+	       
+		ArrayList<byte[]> splitedList = BytesUtil.split(test1.getBytes(), "|||".getBytes());
+	       
+		assertTrue(splitedList.size() == 2);
+		assertTrue(new String(splitedList.get(0)).equals("123"));
+		assertTrue(new String(splitedList.get(1)).equals("6789"));
+	}
+}
