1ad4df955afba8e4d5a47b866513e1c8b4491e44
gromit25 <gromit25@hanmail.net>
2023-03-01 12:39:46 +0900

    escape 처리시 유니코드(\u0000) 추가

--- src/main/java/com/jutools/StringUtil.java
+++ src/main/java/com/jutools/StringUtil.java
@@ -13,18 +13,22 @@ public class StringUtil {
 	 * @param str 주어진 문자열
 	 * @return 이스케이프 처리된 문자열
 	 */
-	public static String escape(String str) {
+	public static String escape(String str) throws Exception {
 		
 		// 입력값 검증
 		if(str == null) {
 			return null;
 		}
 		
-		// 이스케이프 처리된 문자열 버프 변수
-		StringBuffer buffer = new StringBuffer("");
+		// 이스케이프 처리된 문자열 변수
+		StringBuilder escapedStr = new StringBuilder("");
+		
+		// 유니코드 임시 저장 변수
+		StringBuilder unicodeStr = new StringBuilder(""); 
 		
 		// 이스케이프 처리를 위한 상태 변수
-		// 0: 문자열, 1: 이스케이프 문자 
+		// 0: 문자열, 1: 이스케이프 문자,
+		// 10:유니코드 시작, 11:유니코드 1번째 문자, 12:유니코드 2번째 문자, 13:유니코드 3번째 문자, 14:유니코드 4번째 문자 
 		int status = 0;
 		
 		for(int index = 0; index < str.length(); index++) {
@@ -36,25 +40,77 @@ public class StringUtil {
 				if(ch == '\\') {
 					status = 1;
 				} else {
-					buffer.append(ch);
+					escapedStr.append(ch);
+				}
+				
+			} else if(status == 1) {
+				
+				// 상태를 일반 문자열 상태로 설정
+				// 먼저 상태를 변경하는 이유는 Unicode 시작시 상태가 10으로 변경하기 때문에
+				// 마지막에 상태를 변경하면 안됨
+				status = 0;
+				
+				if(ch == '0') {
+					escapedStr.append('\0'); // ASCII 0 추가
+				} else if(ch == 'b') {
+					escapedStr.append('\b');
+				} else if(ch == 'f') {
+					escapedStr.append('\f');
+				} else if(ch == 'n') {
+					escapedStr.append('\n');
+				} else if(ch == 'r') {
+					escapedStr.append('\r');
+				} else if(ch == 't') {
+					escapedStr.append('\t');
+				} else if(ch == 'u') {
+					// Unicode 시작
+					status = 10;
+				} else {
+					// 없을 경우 해당 문자를 그냥 추가함
+					// ex) \' 인경우 '를 추가
+					escapedStr.append(ch);
+				}
+				
+			} else if(status >= 10 && status <= 14) {
+				
+				// ch가 16진수 값(0-9, A-F, a-f) 인지 확인
+				if(isHex(ch) == false) {
+					throw new Exception("unicode value is invalid:" + ch);
+				}
+				
+				// unicode 버퍼에 ch추가
+				unicodeStr.append(ch);
+
+				// 상태값을 하나 올림
+				// ex) 10:유니코드 시작 -> 11:유니코드 1번째 문자
+				status++;
+				
+				// Unicode escape가 종료(status가 14 이상)되면
+				// Unicode를 추가하고, 상태를 일반문자열 상태로 변경함
+				if(status >= 15) {
+					
+					char unicodeCh = (char)Integer.parseInt(unicodeStr.toString(), 16);
+					escapedStr.append(unicodeCh);
+					
+					unicodeStr.delete(0, unicodeStr.length());
+					status = 0;
 				}
 				
 			} else {
-				
-				if(ch == 't') {
-					buffer.append('\t');
-				} else if(ch == 'r') {
-					buffer.append('\r');
-				} else if(ch == 'n') {
-					buffer.append('\n');
-				} else {
-					buffer.append(ch);
-				}
-				
-				status = 0;
+				throw new Exception("Unexpected status: " + status);
 			}
 		} // End of for
 		
-		return buffer.toString();
+		return escapedStr.toString();
+	}
+	
+	/**
+	 * 주어진 문자(ch)가 16진수 값(0-9, A-F, a-f) 인지 확인
+	 * 
+	 * @param ch 검사할 문자
+	 * @return 16진수 값 여부(16진수 값일 경우 true, 아닐 경우 false)
+	 */
+	public static boolean isHex(char ch) {
+		return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F');
 	}
 }
