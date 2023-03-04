d26d3ba1ceb44d22d5954d89ad3f2a14cd9dfd26
gromit25 <gromit25@hanmail.net>
2023-03-04 11:34:30 +0900

    사용하지 않는 import 삭제

--- src/test/java/com/jutools/BytesUtilTest.java
+++ src/test/java/com/jutools/BytesUtilTest.java
@@ -4,7 +4,6 @@
 package com.jutools;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
