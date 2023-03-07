53972abc3476afc7cd035c828a03128bf4ed0f9d
jmsohn <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>
2023-03-07 15:47:52 +0900

    수학 관련 메소드 분리(StringUtil -> MathUtil)

---
+++ src/main/java/com/jutools/MathUtil.java
@@ -0,0 +1,155 @@
+package com.jutools;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
+import com.jutools.mathexp.instructions.Instructions;
+
+public class MathUtil {
+	
+	private static HashMap<String, Double> unitPrefixMap;
+	
+	static {
+		
+		unitPrefixMap = new HashMap<String, Double>();
+		
+		// https://en.wikipedia.org/wiki/Metric_prefix
+		unitPrefixMap.put("Q", 1000000000000000000000000000000.0);
+		unitPrefixMap.put("R", 1000000000000000000000000000.0);
+		unitPrefixMap.put("Y", 1000000000000000000000000.0);
+		unitPrefixMap.put("Z", 1000000000000000000000.0);
+		unitPrefixMap.put("E", 1000000000000000000.0);
+		unitPrefixMap.put("P", 1000000000000000.0);
+		unitPrefixMap.put("T", 1000000000000.0);
+		unitPrefixMap.put("G", 1000000000.0);
+		unitPrefixMap.put("M", 1000000.0);
+		unitPrefixMap.put("k", 1000.0);
+		unitPrefixMap.put("h", 100.0);
+		unitPrefixMap.put("da", 10.0);
+		unitPrefixMap.put("d", 0.1);
+		unitPrefixMap.put("c", 0.01);
+		unitPrefixMap.put("m", 0.001);
+		unitPrefixMap.put("μ", 0.000001);
+		unitPrefixMap.put("n", 0.000000001);
+		unitPrefixMap.put("p", 0.000000000001);
+		unitPrefixMap.put("f", 0.000000000000001);
+		unitPrefixMap.put("a", 0.000000000000000001);
+		unitPrefixMap.put("z", 0.000000000000000000001);
+		unitPrefixMap.put("y", 0.000000000000000000000001);
+		unitPrefixMap.put("r", 0.000000000000000000000000001);
+		unitPrefixMap.put("q", 0.000000000000000000000000000001);
+		
+		// https://en.wikipedia.org/wiki/Binary_prefix
+		unitPrefixMap.put("Ki", 1024.0);
+		unitPrefixMap.put("Mi", 1048576.0);
+		unitPrefixMap.put("Gi", 1073741824.0);
+		unitPrefixMap.put("Ti", 1099511627776.0);
+		unitPrefixMap.put("Pi", 1125899906842624.0);
+		unitPrefixMap.put("Ei", 1152921504606846976.0);
+		unitPrefixMap.put("Zi", 1180591620717411303424.0);
+		unitPrefixMap.put("Yi", 1208925819614629174706176.0);
+		
+	}
+	
+	/**
+	 * 
+	 * 참조)<br>
+	 * https://en.wikipedia.org/wiki/Metric_prefix<br>
+	 * https://en.wikipedia.org/wiki/Binary_prefix
+	 * 
+	 * @param unitPrefix
+	 * @return
+	 */
+	public static double unitPrefixToFactor(String unitPrefix) throws Exception {
+		
+		if(unitPrefixMap != null && unitPrefixMap.containsKey(unitPrefix) == true) {
+			return unitPrefixMap.get(unitPrefix);
+		} else if(unitPrefix != null && unitPrefix.trim().equals("") == true) {
+			return 1;
+		} else {
+			throw new Exception("requested unit prefix(" + unitPrefix + ") is not found");
+		}
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param value
+	 * @param unitPrefix
+	 * @return
+	 */
+	public static double toFullValue(double value, String unitPrefix) throws Exception {
+		return value * unitPrefixToFactor(unitPrefix); 
+	}
+	
+	
+	/**
+	 * 
+	 * 
+	 * @param value
+	 * @param unitPrefix
+	 * @return
+	 */
+	public static double toUnitValue(double value, String unitPrefix) throws Exception {
+		return value / unitPrefixToFactor(unitPrefix); 
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param exp
+	 * @return
+	 */
+	public static double calculate(String exp) throws Exception {
+		return Instructions.calculate(exp);
+	}
+	
+	/**
+	 * 천자리 콤마 추가
+	 * 
+	 * @param value 콤마 추가할 대상
+	 * @return 천자리 콤마 추가된 문자열
+	 */
+	public static String toThousandCommaStr(int value) {
+		DecimalFormat df = new DecimalFormat("###,###");
+		return df.format(value);
+	}
+	
+	/**
+	 * 천자리 콤마 추가
+	 * 
+	 * @param value 콤마 추가할 대상
+	 * @return 천자리 콤마 추가된 문자열
+	 */
+	public static String toThousandCommaStr(long value) {
+		DecimalFormat df = new DecimalFormat("###,###");
+		return df.format(value);
+	}
+
+	/**
+	 * 천자리 콤마 추가
+	 * 소수점 3자리까지 표시
+	 * 
+	 * @param value 콤마 추가할 대상
+	 * @return 천자리 콤마 추가된 문자열
+	 */
+	public static String toThousandCommaStr(float value) {
+		DecimalFormat df = new DecimalFormat("###,###.###");
+		return df.format(value);
+	}
+	
+	/**
+	 * 천자리 콤마 추가
+	 * 소수점 3자리까지 표시
+	 * 
+	 * @param value 콤마 추가할 대상
+	 * @return 천자리 콤마 추가된 문자열
+	 */
+	public static String toThousandCommaStr(double value) {
+		DecimalFormat df = new DecimalFormat("###,###.###");
+		return df.format(value);
+	}
+
+
+
+}
--- src/main/java/com/jutools/mathexp/MathExp.java
+++ src/main/java/com/jutools/mathexp/MathExp.java
@@ -1,5 +1,125 @@
 package com.jutools.mathexp;
 
-public class MathExp {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
 
+import com.jutools.mathexp.instructions.Instruction;
+import com.jutools.mathexp.parser.TreeNode;
+import com.jutools.mathexp.parser.script.ArithmaticParser;
+
+import lombok.Getter;
+
+public class MathExp {
+	
+	@Getter
+	private Stack<Object> stack;
+	@Getter
+	private HashMap<String, Object> values;
+	
+	/**
+	 * 
+	 * @param stack
+	 * @param values
+	 */
+	private MathExp(Stack<Object> stack, HashMap<String, Object> values) throws Exception {
+		
+		if(stack == null) {
+			throw new NullPointerException("stack is null");
+		}
+		
+		if(values == null) {
+			throw new NullPointerException("values is null");
+		}
+		
+		this.stack = stack;
+		this.values = values;
+	}
+	
+	/**
+	 * 
+	 * 
+	 */
+	private MathExp() throws Exception {
+		this(new Stack<Object>(), new HashMap<String, Object>());
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param exp
+	 * @return
+	 */
+	public static double calculate(String exp, HashMap<String, Object> values) throws Exception {
+		
+		ArithmaticParser parser = new ArithmaticParser();
+		TreeNode<Instruction> insts = parser.parse(exp);
+		
+		return (double)MathExp.create(new Stack<Object>(), values).execute(insts).getResult(); 
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param exp
+	 * @return
+	 */
+	public static double calculate(String exp) throws Exception {
+		return calculate(exp, new HashMap<String, Object>()); 
+	}
+	
+	/**
+	 *
+	 * 
+	 * @return
+	 */
+	public static MathExp create() throws Exception {
+		return new MathExp();
+	}
+	
+	/**
+	 * 
+	 * @param stack
+	 * @param values
+	 * @return
+	 */
+	public static MathExp create(Stack<Object> stack, HashMap<String, Object> values) throws Exception {
+		return new MathExp(stack, values);
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param insts
+	 */
+	public MathExp execute(ArrayList<Instruction> insts) throws Exception {
+		for(Instruction inst: insts) {
+			inst.execute(this.stack, this.values);
+		}
+		
+		return this;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param nodes
+	 * @return
+	 */
+	public MathExp execute(TreeNode<Instruction> nodes) throws Exception {
+		
+		ArrayList<Instruction> insts = nodes.travelPostOrder();
+		this.execute(insts);
+		
+		return this;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	public Object getResult() throws Exception {
+		return this.stack.pop();
+	}
 }
