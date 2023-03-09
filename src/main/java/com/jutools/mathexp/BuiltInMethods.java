b3a7ccb3e9e5b01a04c071dc70af9dc2046f98c8
jmsohn <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>
2023-03-10 08:52:01 +0900

    주석 추가

--- src/main/java/com/jutools/mathexp/BuiltInMethods.java
+++ src/main/java/com/jutools/mathexp/BuiltInMethods.java
@@ -1,17 +1,30 @@
 package com.jutools.mathexp;
 
 /**
- * 
+ * 수식에서 사용될 기본 지원 메소드 클래스
  * 
  * @author jmsohn
  */
 public class BuiltInMethods {
 	
+	/**
+	 * 반올림 메소드
+	 * 
+	 * @param value 반올림 대상
+	 * @return 반올림 수행 결과
+	 */
 	@MethodAlias(alias = "round")
 	public static double round(double value) {
 		return Math.round(value);
 	}
 	
+	/**
+	 * 지수 곱 메소드
+	 * 
+	 * @param base 지수 곱을 할 base 값
+	 * @param exponent 지수 곱 횟수
+	 * @return 지수 곱 결과
+	 */
 	@MethodAlias(alias = "pow")
 	public static double pow(double base, double exponent) {
 		return Math.pow(base, exponent);
--- src/main/java/com/jutools/mathexp/MethodAlias.java
+++ src/main/java/com/jutools/mathexp/MethodAlias.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Method Alias를 설정하는 Annotation<br>
+ * 설정하는 메소드는 public static 이어야 하며,<br>
+ * return type은 double/Double 이어야 함
+ * 
+ * @author jmsohn
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface MethodAlias {
