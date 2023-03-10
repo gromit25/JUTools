e87f577648f502527a6fdc9058c14c5037f642c5
jmsohn <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>
2023-03-10 09:02:27 +0900

    주석 추가

--- src/main/java/com/jutools/mathexp/parser/EndStatusType.java
+++ src/main/java/com/jutools/mathexp/parser/EndStatusType.java
@@ -1,9 +1,17 @@
 package com.jutools.mathexp.parser;
 
+/**
+ * 종료 상태의 종류
+ * 
+ * @author jmsohn
+ */
 public enum EndStatusType {
 	
+	/** 일반적인 종료 상태로 설정된 상태로 종료되면 정상 종료 */
 	NORMAL_END,
+	/** 설정된 상태로 변경되면 파싱을 종료함 */
 	IMMEDIATELY_END,
+	/** 설정된 상태로 변경되면 오류임 */
 	ERROR
 
 }
