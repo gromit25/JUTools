ffecb6d4fad10beb58ab58c9a54dc57f1d30dac5
gromit25 <gromit25@hanmail.net>
2023-03-11 18:11:52 +0900

    결과를 Object에서 double로 변경

--- src/main/java/com/jutools/mathexp/MathResult.java
+++ src/main/java/com/jutools/mathexp/MathResult.java
@@ -11,18 +11,8 @@ import lombok.Data;
 public class MathResult {
 	
 	/** 수식 계산 결과 값 */
-	private Object value;
+	private double value;
 	/** 단위(없을 경우 "") */
 	private String baseUnit;
-	
-	/**
-	 * 설정된 타입(returnType)에 따라 수식 계산 결과 반환
-	 * 
-	 * @param returnType 반환할 타입
-	 * @return 결과 값
-	 */
-	public <T> T getValue(Class<T> returnType) {
-		return returnType.cast(this.value);
-	}
 	
 }
