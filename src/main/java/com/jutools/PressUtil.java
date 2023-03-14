fda4ec3f46c1076994cd7a64e389013a83cdd856
jmsohn <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>
2023-03-14 09:44:40 +0900

    publish util 생성(PressUtil 삭제)

--- src/main/java/com/jutools/PressUtil.java
+++
@@ -1,9 +0,0 @@
-package com.jutools;
-
-public class PressUtil {
-
-	public PressUtil() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
---
+++ src/main/java/com/jutools/PublishUtil.java
@@ -0,0 +1,5 @@
+package com.jutools;
+
+public class PublishUtil {
+
+}
