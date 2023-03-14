e9356fda1cab71c5b30e5eaba6fb4d313e7d0d60
jmsohn <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>
2023-03-14 13:41:41 +0900

    주석 추가

--- src/main/java/com/jutools/instructions/BiNumInstruction.java
+++ src/main/java/com/jutools/instructions/BiNumInstruction.java
@@ -3,16 +3,30 @@ package com.jutools.instructions;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * 숫자 이항 연산 명령어 추상 클래스
+ * 
+ * @author jmsohn
+ */
 public abstract class BiNumInstruction extends Instruction {
 	
+	/**
+	 * 이항 연산 수행
+	 * 
+	 * @param p1 첫번째 파라미터
+	 * @param p2 두번째 파라미터
+	 * @return 연산 결과
+	 */
 	public abstract Object process(Double p1, Double p2) throws Exception;
 
 	@Override
 	public void execute(Stack<Object> stack, Map<String, Object> values) throws Exception {
 		
+		// 파라미터를 스택에서 꺼내옴
 		Object p2 = stack.pop();
 		Object p1 = stack.pop();
 		
+		// p1, p2에 대한 검증 수행
 		if(p2 == null) {
 			throw new NullPointerException("p2 value is null");
 		}
@@ -29,6 +43,8 @@ public abstract class BiNumInstruction extends Instruction {
 			throw new Exception("Unexpected type:" + p1.getClass());
 		}
 		
+		// p1, p2에 대한 연산 수행
+		// 수행결과는 숫자(double) 또는 boolean 값
 		Object result = this.process((Double)p1, (Double)p2);
 		
 		if((result instanceof Double) == false &&
@@ -36,6 +52,7 @@ public abstract class BiNumInstruction extends Instruction {
 			throw new Exception("result value is not expected type:" + result.getClass());
 		}
 		
+		// 스택에 결과 푸시
 		stack.push(result);
 
 	}
