d9f483726578f25b20ac79aa7898f57406bea043
jmsohn <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>
2023-03-20 11:47:15 +0900

    Comparison 테스트 케이스 추가

--- src/test/java/com/jutools/OLExpTest.java
+++ src/test/java/com/jutools/OLExpTest.java
@@ -4,14 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.jutools.olexp.OLExp;
+
 class OLExpTest {
 
 	@Test
 	void testCalculate1_1() {
 		try {
+
+			Boolean result = OLExp.compile("1 > 2")
+								.execute()
+								.pop(Boolean.class);
 			
-			double result = OL.calculate("-123.45");
-			assertEquals(-123.45, result);
+			assertEquals(false, result);
 			
 		} catch(Exception ex) {
 			ex.printStackTrace();
