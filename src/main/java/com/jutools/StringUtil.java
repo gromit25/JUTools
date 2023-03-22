7f4a857b8dc4f6838766430b99e7b76bccbd82b7
gromit25 <gromit25@hanmail.net>
2023-03-22 23:47:56 +0900

    isEmpty 메소드 추가

--- src/main/java/com/jutools/StringUtil.java
+++ src/main/java/com/jutools/StringUtil.java
@@ -412,7 +412,7 @@ public class StringUtil {
 	}
 	
 	/**
-	 * 문자열의 길이를 반환
+	 * 문자열의 길이를 반환하는 메소드<br>
 	 * -> 주어진 문자열이 null 일 경우 0을 반환함
 	 * 
 	 * @param str 문자열
@@ -425,6 +425,22 @@ public class StringUtil {
 		}
 		
 		return str.length();
+	}
+
+	/**
+	 * 문자열이 비어 있는지 반환하는 메소드<br>
+	 * -> 주어진 문자열이 null 일 경우 true를 반환함
+	 * 
+	 * @param str 문자열
+	 * @return 문자열이 비어 있는지 여부
+	 */
+	public static boolean isEmpty(String str) {
+		
+		if(str == null) {
+			return true;
+		}
+		
+		return str.isEmpty();
 	}
 
 }
