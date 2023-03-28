a3af5b7c37ed83bde13ff832aa19cbefff09f9e8
gromit25 <gromit25@hanmail.net>
2023-03-28 21:31:08 +0900

    print formatter 오류 수정

--- src/main/java/com/jutools/publish/formatter/text/PrintFormatter.java
+++ src/main/java/com/jutools/publish/formatter/text/PrintFormatter.java
@@ -4,7 +4,7 @@ import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.Map;
 
-import com.jutools.mathexp.MathExp;
+import com.jutools.olexp.OLExp;
 import com.jutools.publish.formatter.Formatter;
 import com.jutools.publish.formatter.FormatterAttr;
 import com.jutools.publish.formatter.FormatterException;
@@ -29,7 +29,7 @@ public class PrintFormatter extends AbstractTextFormatter {
 	@Getter
 	@Setter
 	@FormatterAttr(name="exp", mandatory=true)
-	private MathExp exp;
+	private OLExp exp;
 	
 	/**
 	 * 출력 길이
@@ -57,19 +57,19 @@ public class PrintFormatter extends AbstractTextFormatter {
 		// 수행 결과 값을 반환함
 		try {
 			
-			Object result = this.getExp().execute(values).getResult().getValue();
+			Object result = this.getExp().execute(values).pop(Object.class);
 			
 			// 수행 결과를 메세지에 설정함
 			if(result != null) {
 				out.write(
-						PublisherUtil.makeString(result.toString(), this.getLength())
+						makeString(result.toString(), this.getLength())
 						// 개행만 있고 '|' 이 없을경우,
 						// 개행 이후 부분을 최종적으로 잘라내기 때문에
 						// '|'으로 변경해 줌
 						.replaceAll("\n", "\n|")
 						.getBytes(charset));
 			} else {
-				out.write(PublisherUtil.makeString("N/A", this.getLength()).getBytes(charset));
+				out.write(makeString("N/A", this.getLength()).getBytes(charset));
 			}
 			
 			out.flush();
@@ -78,6 +78,45 @@ public class PrintFormatter extends AbstractTextFormatter {
 			
 			// 예외 발생시
 			throw new FormatterException(this, ex);
+		}
+		
+	}
+	
+	/**
+	 * print tag에 설정된 형식으로 문자열을 만듦
+	 * -> String.format의 %s를 사용하지 않는 이유는
+	 *    한글의 경우 2byte이지만 1문자로 계산되어
+	 *    실제 원하는 길이가 안맞는 문제 발생
+	 *    ex) "한test", "1test" 문자열에 6칸으로 요청하면
+	 *        -> "한test ","1test " 이렇게 출력되어 console 화면에 표현되는 길이가 맞지 않음
+	 * -> length 가 음수(-)일 경우, 원래 스트링을 반환
+	 * 
+	 * @param target 길이를 맞출 문자열
+	 * @param length 맞출 길이
+	 * @return 길이(PrintFormatter.length)가 맞추어진 문자열
+	 */
+	private static String makeString(String target, int length) {
+		
+		// taget 문자열의 byte 길이를 가져옴
+		int targetLength = 0;
+		if(target != null) {
+			targetLength = target.getBytes().length;
+		}
+		
+		// 설정해야할 길이가 0 보다 작거나,
+		// target 문자열이 더 길 경우,
+		// target문자열을 리턴함
+		if(length <= 0 || length <= targetLength) {
+			return target;
+		}
+		
+		// 추가해야할 space개수를 계산함
+		int countOfSpace = length;
+		if(target != null) {
+			countOfSpace -= targetLength;
+			return target + String.format("%" + countOfSpace + "s", "");
+		} else {
+			return String.format("%" + countOfSpace + "s", "");
 		}
 		
 	}
