137dae3afaae651a774cb5bc3cb8fc3f8683f21e
gromit25 <gromit25@hanmail.net>
2023-05-27 14:36:41 +0900

    주석 추가

--- src/main/java/com/jutools/CronJob.java
+++ src/main/java/com/jutools/CronJob.java
@@ -16,14 +16,14 @@ import lombok.Getter;
  */
 public class CronJob {
 	
-	/** */
+	/** 크론 시간 표현식 */
 	private CronExp cronExp;
-	/** */
+	/** 수행할 잡 */
 	private Runnable job;
 	
-	/** */
+	/** 크론 쓰레드 객체 */
 	private Thread cronThread;
-	/** */
+	/** 잡 쓰레드 객체 */
 	private Thread jobThread;
 	
 	/** 다음 작업 시간: 중지되어 있을 경우 -1 */
@@ -33,8 +33,8 @@ public class CronJob {
 	/**
 	 * 생성자
 	 * 
-	 * @param cronExp
-	 * @param job
+	 * @param cronExp 크론 시간 표현식
+	 * @param job 수행할 잡
 	 */
 	@Builder
 	public CronJob(String cronExp, Runnable job) throws Exception {
@@ -43,17 +43,18 @@ public class CronJob {
 	}
 	
 	/**
-	 * 
+	 * cron job 수행
 	 */
 	public void run() {
 		
 		this.cronThread = new Thread(new Runnable() {
+			
 			@Override
 			public void run() {
 				
 				while(true) {
 					
-					//
+					// 다음 수행 시간까지 대기
 					try {
 						nextTime = cronExp.getNextTimeInMillis();
 						Thread.sleep(nextTime - System.currentTimeMillis());
@@ -61,7 +62,7 @@ public class CronJob {
 						break;
 					}
 					
-					//
+					// 
 					jobThread = new Thread(job);
 					jobThread.start();
 				}
@@ -89,26 +90,27 @@ public class CronJob {
 	}
 	
 	/**
+	 * 크론 시간 표현식 설정
 	 * 
-	 * 
-	 * @param cronExp
+	 * @param cronExp 크론 시간 표현식 문자열
 	 */
 	public void setCronExp(String cronExp) throws Exception {
 		this.cronExp = new CronExp(cronExp);
 	}
 	
 	/**
+	 * 설정된 크론 시간 표현식 반환
 	 * 
-	 * @return
+	 * @return 설정된 시간 표현식
 	 */
 	public String getCronExp() {
 		return this.cronExp.getCronExp();
 	}
 	
 	/**
+	 * 수행할 잡 설정
 	 * 
-	 * 
-	 * @param job
+	 * @param job 수행할 잡
 	 */
 	public void setJob(Runnable job) {
 		this.job = job;
