fae66c0765ea9b302e55b945778ca43cf409a9f3
gromit25 <gromit25@hanmail.net>
2023-07-17 09:23:58 +0900

    메소드 명 수정 및 주석 추가

--- src/main/java/com/jutools/StatUtil.java
+++ src/main/java/com/jutools/StatUtil.java
@@ -42,14 +42,25 @@ public class StatUtil {
 	}
 	
 	/**
-	 * 넬슨룰 검사 객체 생성
+	 * 넬슨룰 검사 객체 생성<br>
+	 * 사용법)
+	 * <pre> 
+	 * NelsonRule nrule = StatUtil.createNelsonRule(10, 1, NelsonRule.RULE1|NelsonRule.RULE2);
+	 * 
+	 * for(double value: dataList) {
+	 *     ArrayList&lt;Integer&gt; violatedRules = nrule.check(value);
+	 *     for(Integer ruleIndex: violatedRules) {
+	 *         // 넬슨룰 위반 처리
+	 *     }
+	 * }
+	 * </pre>
 	 * 
 	 * @param mean 평균
 	 * @param std 표준편차
 	 * @param rules 검사할 넬슨룰 목록 비트맵
 	 * @return
 	 */
-	public static NelsonRule createNelsonRuleChecker(double mean, double std, int rules) throws Exception {
+	public static NelsonRule createNelsonRule(double mean, double std, int rules) throws Exception {
 		return NelsonRule.builder()
 				.mean(10)
 				.std(1)
