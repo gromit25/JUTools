a29fcf55fdcbb5b6ce5212cccfc54f919862039f
jmsohn <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>
2023-08-29 13:05:57 +0900

    문자열 덧셈 가능하도록 수정

--- src/main/java/com/jutools/instructions/ADD.java
+++ src/main/java/com/jutools/instructions/ADD.java
@@ -1,15 +1,50 @@
 package com.jutools.instructions;
 
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * 덧셈 명령어 클래스
  * 
  * @author jmsohn
  */
-public class ADD extends BiNumInstruction {
+public class ADD extends Instruction {
 
 	@Override
-	public Object process(Double p1, Double p2) throws Exception {
-		return p1 + p2;
+	public void execute(Stack<Object> stack, Map<String, Object> values) throws Exception {
+		
+		// 파라미터를 스택에서 꺼내옴
+		Object p2 = stack.pop();
+		Object p1 = stack.pop();
+		
+		// p1, p2에 대한 검증 수행
+		if(p2 == null) {
+			throw new NullPointerException("p2 value is null");
+		}
+		
+		if(p1 == null) {
+			throw new NullPointerException("p1 value is null");
+		}
+		
+		// 결과 변수
+		Object result = null; 
+		
+		// p1, p2가 모두 Double 형일 경우, 덧샘 수행
+		// 아닐 경우 문자열 더하기 연산 수행
+		if((p1 instanceof Double) == true && (p2 instanceof Double) == true) {
+			
+			// p1, p2에 대한 덧셈 연산 수행
+			result = ((Double)p1) + ((Double)p2);
+
+		} else {
+
+			// p1, p2의 문자열을 합침
+			result = p1.toString() + p2.toString();
+		
+		}
+		
+		// 스택에 결과 푸시
+		stack.push(result);
 	}
 
 }
