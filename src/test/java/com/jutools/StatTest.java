72653e1757cb68279578171270a6a541b45f1133
gromit25 <gromit25@hanmail.net>
2023-09-03 10:55:08 +0900

    테스트 케이스 추가

--- src/test/java/com/jutools/StatTest.java
+++ src/test/java/com/jutools/StatTest.java
@@ -10,7 +10,7 @@ import com.jutools.stat.RTStatistic;
 public class StatTest {
 
 	@Test
-	public void testRTParameter1() throws Exception {
+	public void testRTParameter1_1() throws Exception {
 		
 		double[] values = {10, 12, 14, 18, 20, 22, 24, 25, 28, 30};
 		
@@ -27,7 +27,21 @@ public class StatTest {
 	}
 	
 	@Test
-	public void testRTStatistic1() throws Exception {
+	public void testRTParameter2_1() throws Exception {
+		
+		RTParameter stat = new RTParameter(203, 4533, 108353, 2715441, 10);
+		
+		assertEquals(10, stat.getCount());
+		assertEquals(203.0, stat.getSum(), 0.0);
+		assertEquals(20.3, stat.getMean(), 0.0);
+		assertEquals(41.21, stat.getVariance(), 0.01);
+		assertEquals(6.419, stat.getStd(), 0.01);
+		assertEquals(-0.1505, stat.getSkewness(), 0.01);
+		assertEquals(-1.1948, stat.getKurtosis(), 0.01);
+	}
+	
+	@Test
+	public void testRTStatistic1_1() throws Exception {
 		
 		double[] values = {10, 12, 14, 18, 20, 22, 24, 25, 28, 30};
 		
@@ -44,7 +58,7 @@ public class StatTest {
 	}
 	
 	@Test
-	public void testRTStatistic2() throws Exception {
+	public void testRTStatistic1_2() throws Exception {
 		
 		double[] values = {10};
 		
@@ -58,6 +72,20 @@ public class StatTest {
 		assertEquals(0.0, stat.getStd(), 0.01);
 		assertEquals(0.0, stat.getSkewness(), 0.01);
 		assertEquals(0.0, stat.getKurtosis(), 0.01);
+	}
+	
+	@Test
+	public void testRTStatistic2_1() throws Exception {
+		
+		RTStatistic stat = new RTStatistic(203, 4533, 108353, 2715441, 10);
+		
+		assertEquals(10, stat.getCount());
+		assertEquals(203.0, stat.getSum(), 0.0);
+		assertEquals(20.3, stat.getMean(), 0.0);
+		assertEquals(45.7888, stat.getVariance(), 0.01);
+		assertEquals(6.7667, stat.getStd(), 0.01);
+		assertEquals(-0.1784, stat.getSkewness(), 0.01);
+		assertEquals(-1.1480, stat.getKurtosis(), 0.01);
 	}
 
 }
