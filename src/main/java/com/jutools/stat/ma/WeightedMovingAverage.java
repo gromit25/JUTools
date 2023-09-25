71f9a219c23368b5794b066fd87a40e12de08b5e
gromit25 <gromit25@hanmail.net>
2023-09-25 21:33:37 +0900

    Weighted Moving Average 개발 중

--- src/main/java/com/jutools/stat/ma/WeightedMovingAverage.java
+++ src/main/java/com/jutools/stat/ma/WeightedMovingAverage.java
@@ -5,16 +5,41 @@ package com.jutools.stat.ma;
  * @author jmsohn
  */
 public class WeightedMovingAverage extends AbstractMovingAverage {
+	
+	private double[] weights;
+	private double sumOfWeights;
 
-	public WeightedMovingAverage(int windowSize) throws Exception {
-		super(windowSize);
-		// TODO Auto-generated constructor stub
+	/**
+	 * 
+	 * @param weights
+	 */
+	public WeightedMovingAverage(double[] weights) throws Exception {
+		
+		super(weights.length);
+		
+		this.weights = weights;
+		
+		this.sumOfWeights = 0.0;
+		for(double weight: this.weights) {
+			this.sumOfWeights += weight;
+		}
+		
+		if(this.sumOfWeights == 0) {
+			throw new IllegalArgumentException("sum of weights must be none zero");
+		}
 	}
 
 	@Override
 	protected double calculate(double in, double out) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		double sum = 0.0;
+		int index = this.weights.length - this.getQueue().size();
+		
+		for(double value: this.getQueue()) {
+			sum += value * this.weights[index];
+		}
+		
+		return sum / this.sumOfWeights;
 	}
 
 }
