5c768927a4132c8212acad054f5a9c04ff0e44da
gromit25 <gromit25@hanmail.net>
2023-09-30 08:57:52 +0900

    주석 추가 및 메소드에 예외 추가

--- src/main/java/com/jutools/cache/Loader.java
+++ src/main/java/com/jutools/cache/Loader.java
@@ -1,8 +1,19 @@
 package com.jutools.cache;
 
+/**
+ * 캐시에 데이터를 제공할 인터페이스
+ * 
+ * @param <T> 반환 타입
+ */
 @FunctionalInterface
 public interface Loader<T> {
 	
-	public T get(String key);
+	/**
+	 * 주어진 키에 해당하는 데이터를 반환
+	 * 
+	 * @param key 키
+	 * @return 데이터
+	 */
+	public T get(String key) throws Exception;
 
 }
