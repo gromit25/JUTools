c4057daddc3bca43400b870ac43878d7ca0b9e5a
gromit25 <gromit25@hanmail.net>
2023-09-30 11:41:27 +0900

    디폴트 캐시 보유기간 1분 -> 10분 , 디폴트 캐시 정리기간 1분 -> 10분으로 변경

--- src/main/java/com/jutools/CacheService.java
+++ src/main/java/com/jutools/CacheService.java
@@ -25,26 +25,26 @@ public class CacheService {
 	
 	/**
 	 * 캐시 객체 생성<br>
-	 * 1분 단위로 캐시 정리 
+	 * 10분 단위로 캐시 정리 
 	 * 
 	 * @param loader 캐시 데이터 공급 객체
 	 * @param retainTime 캐시 보유기간, 단위: millisecond
 	 * @return 캐시 객체
 	 */
 	public static <T> Cache<T> create(Loader<T> loader, long retainTime) throws Exception {
-		return create(loader, retainTime, "* * * * *");
+		return create(loader, retainTime, "*/10 * * * *");
 	}
 	
 	/**
 	 * 캐시 객체 생성<br>
-	 * 캐시 보유 시간 1분<br>
-	 * 1분 단위로 캐시 정리
+	 * 캐시 보유 시간 10분<br>
+	 * 10분 단위로 캐시 정리
 	 * 
 	 * @param loader 캐시 데이터 공급 객체
 	 * @return 캐시 객체
 	 */
 	public static <T> Cache<T> create(Loader<T> loader) throws Exception {
-		return create(loader, 60 * 1000);
+		return create(loader, 10 * 60 * 1000);
 	}
 
 }
