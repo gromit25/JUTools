7ff8d24b8fec023d0159dadfd94e33cc6602c83c
gromit25 <gromit25@hanmail.net>
2023-10-06 18:26:21 +0900

    와일드 카드, 정규 표현식 매치 메소드 추가

--- src/main/java/com/jutools/instructions/BuiltInMethods.java
+++ src/main/java/com/jutools/instructions/BuiltInMethods.java
@@ -1,5 +1,7 @@
 package com.jutools.instructions;
 
+import com.jutools.StringUtil.WildcardPattern;
+
 /**
  * built in method 클래스
  * 
@@ -28,5 +30,29 @@ public class BuiltInMethods {
 	@MethodAlias(alias = "pow")
 	public static double pow(double base, double exponent) {
 		return Math.pow(base, exponent);
+	}
+	
+	/**
+	 * 와일드 카드 매치 메소드
+	 * 
+	 * @param pattern 와일드 카드 패턴
+	 * @param target 매치할 문자열
+	 * @return 와일드 카드 매치 여부
+	 */
+	@MethodAlias(alias = "matchW")
+	public static boolean matchWildcard(String pattern, String target) throws Exception {
+		return WildcardPattern.create(pattern).match(target).isMatch();
+	}
+	
+	/**
+	 * 정규 표현식 매치 메소드
+	 * 
+	 * @param pattern 정규 표현식
+	 * @param target 매치할 문자열
+	 * @return 정규 표현식 매치 여부
+	 */
+	@MethodAlias(alias = "matchR")
+	public static boolean matchRegExp(String pattern, String target) throws Exception {
+		return target.matches(pattern);
 	}
 }
