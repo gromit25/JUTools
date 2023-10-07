65daf1ec822d17c6964b789d6a19cb7539ff4a71
gromit25 <gromit25@hanmail.net>
2023-10-07 13:14:48 +0900

    주석 추가

--- src/main/java/com/jutools/publish/formatter/Loc.java
+++ src/main/java/com/jutools/publish/formatter/Loc.java
@@ -3,15 +3,25 @@ package com.jutools.publish.formatter;
 import lombok.Data;
 
 /**
+ * XML 파싱 위치 정보 클래스
  * 
  * @author jmsohn
  */
 @Data
 public class Loc implements Comparable<Loc>{
 	
+	/** XML 라인 번호 */
 	private int lineNum;
+	
+	/** XML 컬럼 번호 */
 	private int columnNum;
 	
+	/**
+	 * 생성자
+	 * 
+	 * @param lineNum XML 라인 번호 초기값
+	 * @param columnNum XML 컬럼 번호 초기값
+	 */
 	public Loc(int lineNum, int columnNum) {
 		this.setLineNum(lineNum);
 		this.setColumnNum(columnNum);
@@ -19,15 +29,22 @@ public class Loc implements Comparable<Loc>{
 
 	@Override
 	public int compareTo(Loc o) {
+
+		// 다른 Loc 객체와 비교
 		
+		// 다른 Loc 객체가 null 이면, 1을 반환
 		if(null == o) {
 			return 1;
 		}
 		
+		// 라인 비교
+		// 현재 라인이 더 크면, 1을 비교
 		if(this.getLineNum() > o.getLineNum()) {
 			return 1;
 		}
 		
+		// 컬럼 비교
+		// 라인이 같고, 컬럼이 현재 컬럼이 크면 1을 반환 
 		if(this.getLineNum() == o.getLineNum()) {
 			if(this.getColumnNum() > o.getColumnNum()) {
 				return 1;
