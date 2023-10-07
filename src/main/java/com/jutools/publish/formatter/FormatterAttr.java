249462fd312965880d522bb9b6fae5c30e627747
gromit25 <gromit25@hanmail.net>
2023-10-07 13:09:19 +0900

    주석 수정

--- src/main/java/com/jutools/publish/formatter/FormatterAttr.java
+++ src/main/java/com/jutools/publish/formatter/FormatterAttr.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Formatter 속성 필드 클래스<br>
+ * Formatter 속성 필드 Annotation<br>
  * xml의 formatter tag의 속성과 formatter 클래스의 필드 속성을 매칭함
  * 
  * @author jmsohn
