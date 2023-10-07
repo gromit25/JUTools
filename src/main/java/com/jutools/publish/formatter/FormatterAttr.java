284784d1ee4b925733c822c6caf3a9f271d95a38
gromit25 <gromit25@hanmail.net>
2023-10-07 12:58:46 +0900

    주석 추가

--- src/main/java/com/jutools/publish/formatter/FormatterAttr.java
+++ src/main/java/com/jutools/publish/formatter/FormatterAttr.java
@@ -5,11 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Formatter 속성 필드 클래스<br>
+ * xml의 formatter tag의 속성과 formatter 클래스의 필드 속성을 매칭함
+ * 
+ * @author jmsohn
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface FormatterAttr {
 	
+	/** xml의 formatter tag의 속정 */
 	public String name();
+	/** 필수 여부 */
 	public boolean mandatory() default true;
 
 }
