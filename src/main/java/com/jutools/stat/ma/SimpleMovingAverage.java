7e8e994c498cb754301f1d3f907f4c411e069b7b
gromit25 <gromit25@hanmail.net>
2023-11-28 22:19:03 +0900

    주석 추가

--- src/main/java/com/jutools/stat/ma/SimpleMovingAverage.java
+++ src/main/java/com/jutools/stat/ma/SimpleMovingAverage.java
@@ -1,9 +1,20 @@
 package com.jutools.stat.ma;
 
+/**
+ * 단순이동평균(Simple Moving Average) 계산 클래스
+ * 
+ * @author jmsohn
+ */
 public class SimpleMovingAverage extends AbstractMovingAverage {
 	
+	/** 구간 합계 */
 	private double sum;
 
+	/**
+	 * 생성자 
+	 * 
+	 * @param windowSize
+	 */
 	public SimpleMovingAverage(int windowSize) throws Exception {
 		super(windowSize);
 	}
@@ -19,5 +30,4 @@ public class SimpleMovingAverage extends AbstractMovingAverage {
 			return sum / (double)this.getWindowSize();
 		}
 	}
-
 }
