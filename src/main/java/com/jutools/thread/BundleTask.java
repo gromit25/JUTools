335b7097b604883d4d509b43ae786a9460196a8c
gromit25 <gromit25@hanmail.net>
2023-12-24 21:59:48 +0900

    주석 추가

--- src/main/java/com/jutools/thread/BundleTask.java
+++ src/main/java/com/jutools/thread/BundleTask.java
@@ -3,7 +3,7 @@ package com.jutools.thread;
 import java.util.List;
 
 /**
- * 
+ * 각 묶음별 처리 정의 클래스
  * 
  * @author jmsohn
  */
@@ -11,11 +11,14 @@ import java.util.List;
 public interface BundleTask {
 	
 	/**
+	 * 각 묶음별 처리 정의 메소드
+	 * -> 주의) 목록이 총 20개(0~19)이고 2개로 묶음으로 나누어질 경우,
+	 *         start:0 ~ end:10, start:10 ~ end:20 으로 나누어짐 
 	 * 
-	 * @param list
-	 * @param bundleId
-	 * @param start
-	 * @param end
+	 * @param list 전체 데이터 목록
+	 * @param bundleId 현재 묶음의 번호 
+	 * @param start 시작 번호 - 데이터 목록에서 시작할 번호
+	 * @param end 끝 번호 - 데이터 목록에서 끝 번호
 	 */
 	public void consume(List<?> list, int bundleId, int start, int end);
 }
