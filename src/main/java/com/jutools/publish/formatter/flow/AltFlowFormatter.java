7e398a17087a586a8f8fb9bcce796a1089913f80
gromit25 <gromit25@hanmail.net>
2023-12-29 20:29:14 +0900

    주석 수정

--- src/main/java/com/jutools/publish/formatter/flow/AltFlowFormatter.java
+++ src/main/java/com/jutools/publish/formatter/flow/AltFlowFormatter.java
@@ -6,9 +6,9 @@ import com.jutools.publish.formatter.FormatterException;
 import com.jutools.publish.formatter.FormatterSpec;
 
 /**
- * 대체(alt) flow formatter
- * foreach 등 대체(alternative) flow가 있는 경우 사용되며,
- * alt 테그의  내용을 처리함
+ * 대체(alternative) flow formatter<br>
+ * foreach 등 대체 flow(List의 0개인 경우 다른 메시지 표시 등)가 있는 경우 사용되며,<br>
+ * alt 테그의  내용을 처리함<br>
  * FlowFormatter와 기능 상 완전 동일함  
  * 
  * @author jmsohn
