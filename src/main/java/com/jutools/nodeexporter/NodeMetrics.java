a22592ad9d55ff70840784cca41fc3e2398a0efa
gromit25 <gromit25@hanmail.net>
2024-01-09 21:37:12 +0900

    주석 추가

--- src/main/java/com/jutools/nodeexporter/NodeMetrics.java
+++ src/main/java/com/jutools/nodeexporter/NodeMetrics.java
@@ -5,25 +5,27 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
+ * Node Exporter의 Metrics 클래스
  * 
  * @author jmsohn
  */
 public class NodeMetrics implements Iterable<NodeMetric>{
 	
-	/** */
+	/** metrics 목록 */
 	private List<NodeMetric> metrics;
 	
 	/**
-	 * 
+	 * 생성자
 	 */
 	NodeMetrics() {
 		this.metrics = new ArrayList<>();
 	}
 	
 	/**
+	 * metric 추가
 	 * 
-	 * @param metric
-	 * @return
+	 * @param metric 추가할 metric
+	 * @return 현재 객체
 	 */
 	NodeMetrics add(NodeMetric metric) throws Exception {
 		
@@ -37,18 +39,19 @@ public class NodeMetrics implements Iterable<NodeMetric>{
 	}
 	
 	/**
+	 * metrics의 개수 반환
 	 * 
-	 * @return
+	 * @return metrics의 개수
 	 */
 	public int size() {
 		return this.metrics.size();
 	}
 	
 	/**
+	 * 특정 인덱스의 metric 반환
 	 * 
-	 * 
-	 * @param index
-	 * @return
+	 * @param index 인덱스
+	 * @return 특정 인덱스의 metric
 	 */
 	public NodeMetric get(int index) throws Exception {
 		
@@ -64,8 +67,9 @@ public class NodeMetrics implements Iterable<NodeMetric>{
 	}
 	
 	/**
+	 * metrics의 value 합 반환
 	 * 
-	 * @return
+	 * @return metrics의 value 합
 	 */
 	public double sumValue() {
 		
@@ -81,5 +85,14 @@ public class NodeMetrics implements Iterable<NodeMetric>{
 	@Override
 	public Iterator<NodeMetric> iterator() {
 		return this.metrics.iterator();
+	}
+	
+	/**
+	 * 
+	 * @param condition
+	 * @return
+	 */
+	public NodeMetrics find(String condition) throws Exception {
+		return null;
 	}
 }
