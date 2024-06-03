ff6bfa7426b9586a1295a2e2dda12ad3f6d57189
gromit25 <gromit25@hanmail.net>
2024-06-04 08:21:16 +0900

    주석 추가

--- src/main/java/com/jutools/parserfw/exception/UnexpectedEndException.java
+++ src/main/java/com/jutools/parserfw/exception/UnexpectedEndException.java
@@ -3,27 +3,28 @@ package com.jutools.parserfw.exception;
 import lombok.Getter;
 
 /**
+ * 파싱 수행시 완료상태가 아닌데 문자열이 끝난 경우 예외 클래스
  * 
  * @author jmsohn
  */
 public class UnexpectedEndException extends Exception {
 
-	/** */
+	/** Serializable GUID */
 	private static final long serialVersionUID = 9217861306987755508L;
 	
 	/** 예외 발생 위치 */
 	@Getter
 	private int pos;
 	
-	/** 예외 발생시 상태 */
+	/** 예외 발생 상태 */
 	@Getter
 	private String status;
 	
 	/**
+	 * 생성자
 	 * 
-	 * @param pos
-	 * @param ch
-	 * @param status
+	 * @param pos 예외 발생 위치
+	 * @param status 예외 발생 상태
 	 */
 	public UnexpectedEndException(int pos, String status) {
 		
@@ -34,10 +35,11 @@ public class UnexpectedEndException extends Exception {
 	}
 	
 	/**
+	 * 예외 정보에 대해 문자열로 변환
 	 * 
-	 * @param pos
-	 * @param status
-	 * @return
+	 * @param pos 예외 발생 위치
+	 * @param status 예외 발생 상태
+	 * @return 변환된 문자열
 	 */
 	private static String toString(int pos, String status) {
 		return "Unexpected end status: " + status + " at " + pos;
