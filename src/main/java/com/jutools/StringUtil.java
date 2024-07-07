4e936faaa706c371c6c42ed091ecd54df01fe6e0
gromit25 <gromit25@hanmail.net>
2024-07-07 20:12:43 +0900

    주석 수정

--- src/main/java/com/jutools/StringUtil.java
+++ src/main/java/com/jutools/StringUtil.java
@@ -451,7 +451,7 @@ public class StringUtil {
 
 	/**
 	 * 문자열이 비어 있는지 반환하는 메소드<br>
-	 * -> 주어진 문자열이 null 일 경우 true를 반환함
+	 * -> 주어진 문자열이 null이거나 "" 일 경우 true를 반환함
 	 * 
 	 * @param str 문자열
 	 * @return 문자열이 비어 있는지 여부
