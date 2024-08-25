456c4c978a621c8f3c2aca49553795a7889caacb
gromit25 <gromit25@hanmail.net>
2024-08-25 14:14:51 +0900

    주석 추가

--- src/main/java/com/jutools/stat/StatisticService.java
+++ src/main/java/com/jutools/stat/StatisticService.java
@@ -4,23 +4,27 @@ import com.jutools.CronJob;
 import com.jutools.StringUtil;
 
 /**
- * 
+ * 통계량 서비스 클래스<br>
+ * 수집 저장 주기에 따라 데이터를 수집 메소드 콜백 후 통계량 계산<br>
+ * 계산 결과에 대해 저잘할 수 있도록 저장 메소드 콜백<br>
+ * 리셋 주기에 저장된 통계량의 값을 모두 삭제함<br>
+ * 만일, 수집 주기와 리셋 주기가 겹칠 경우 수집 저장 후 리셋을 수행함
  * 
  * @author jmsohn
  */
 public class StatisticService {
 	
-	/** */
+	/** 통계량 계산 객체 */
 	private Statistic stats;
 	
-	/** */
+	/** 수집 주기 객체 */
 	private CronJob acquisitorCron;
-	/** */
+	/** 리셋 주기 객체 */
 	private CronJob resetCron;
 	
-	/** */
+	/** 데이터 수집 객체 */
 	private DataAcquistor acquisitor;
-	/** */
+	/** 데이터 저장 객체 */
 	private DataLoader loader;
 	
 	/**
@@ -30,17 +34,17 @@ public class StatisticService {
 	boolean isLock = false;
 	
 	/**
+	 * 통계량 서비스 객체 생성자
 	 * 
-	 * 
-	 * @param acquisitorCronExp
-	 * @param resetCronExp
-	 * @param acquisitor
-	 * @param loader
+	 * @param acquisitorCronExp 데이터 수집 주기 설정 문자열
+	 * @param resetCronExp 데이터 리셋 주기 설정 문자열
+	 * @param acquisitor 데이터 수집 객체
+	 * @param loader 데이터 저장 객체 - optional, null일 경우 수행하지 않음
 	 */
 	public StatisticService(String acquisitorCronExp, String resetCronExp
 			, DataAcquistor acquisitor, DataLoader loader) throws Exception {
 		
-		//
+		// 입력값 검증
 		if(StringUtil.isBlank(acquisitorCronExp) == true) {
 			throw new IllegalArgumentException("acquisitor cron exp is null or blank.");
 		}
@@ -53,25 +57,26 @@ public class StatisticService {
 			throw new IllegalArgumentException("acquisitor is null.");
 		}
 
-		//
+		// 통계량 계산 객체 생성
 		this.stats = new Statistic();
 
-		//
+		// 데이터 수집 객체 설정
 		this.acquisitor = acquisitor;
 		
-		//
+		// 데이터 저장 객체 설정
 		this.loader = loader;
 		
-		//
+		// 수집 주기 객체 생성
 		this.acquisitorCron = new CronJob(acquisitorCronExp, () -> {
 			
-			//
+			// 수집 시작시 수집 락을 설정
 			this.isLock = true;
 			
-			//
+			// 수집 및 저장 실행
 			this.acquireAndLoad();
 			
-			//
+			// 수집 시간과 리셋 시간이 같을 경우
+			// 수집 락을 계속 설정함 -> 수집 저장 이후 리셋을 하도록 하기 위함
 			long acquireTime = this.acquisitorCron.getNextTime();
 			long resetTime = this.resetCron.getNextTime();
 			
@@ -80,7 +85,7 @@ public class StatisticService {
 			}
 		});
 		
-		//
+		// 리셋 주기 객체 설정
 		this.resetCron = new CronJob(resetCronExp, () -> {
 			
 			// acquire 실행 락이 설정되어 있는지 확인
@@ -98,22 +103,22 @@ public class StatisticService {
 	}
 	
 	/**
-	 * 
+	 * 데이터 수집 저장 메소드 콜백
 	 */
 	private void acquireAndLoad() {
 		
-		//
+		// 데이터 수집 메소드 콜백
 		double data = this.acquisitor.acquire();
 		this.stats.add(data);
 		
-		//
+		// 데이터 로드 메소드 콜백
 		if(this.loader != null) {
 			this.loader.load(this.stats);
 		}
 	}
 	
 	/**
-	 * 
+	 * 통계량 초기화
 	 */
 	private void reset() {
 		if(this.stats != null) {
@@ -123,7 +128,7 @@ public class StatisticService {
 	}
 	
 	/**
-	 * 
+	 * 데이터 수집 및 통계량 계산 시작
 	 */
 	public void start() throws Exception {
 		this.acquisitorCron.run();
@@ -131,11 +136,20 @@ public class StatisticService {
 	}
 	
 	/**
-	 * 
+	 * 데이터 수집 및 통계량 수집 중단
 	 */
 	public void stop() throws Exception {
 		this.acquisitorCron.stop();
 		this.resetCron.stop();
+	}
+	
+	/**
+	 * 통계량 객체 반환
+	 * 
+	 * @return 통계량 객체
+	 */
+	public Statistic getStatistic() {
+		return this.stats;
 	}
 	
 	@Override
