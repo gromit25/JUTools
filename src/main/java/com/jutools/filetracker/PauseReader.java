f06a36ee684940451feed76e556f2aaefc9c9bd9
gromit25 <gromit25@hanmail.net>
2025-05-13 22:10:55 +0900

    주석 추가

--- src/main/java/com/jutools/filetracker/PauseReader.java
+++ src/main/java/com/jutools/filetracker/PauseReader.java
@@ -3,18 +3,17 @@ package com.jutools.filetracker;
 import java.util.function.Consumer;
 
 /**
- * 
+ * 끊어 읽기 인터페이스
  * 
  * @author jmsohn
  */
 public interface PauseReader {
 
 	/**
+	 * 끊어 읽기 수행
 	 * 
-	 * 
-	 * @param action
-	 * @param buffer
-	 * @return
+	 * @param buffer 끊어 읽을 데이터 
+	 * @param action 읽은 데이터 처리를 위한 Consumer
 	 */
 	public void read(byte[] buffer, Consumer<String> action) throws Exception;
 }
