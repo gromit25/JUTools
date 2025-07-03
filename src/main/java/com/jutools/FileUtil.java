b4806bb50cbb7af289e499ddff28a7df6c3d7621
gromit25 <gromit25@hanmail.net>
2025-07-03 22:32:48 +0900

    waitForFileTouched 추가

--- src/main/java/com/jutools/FileUtil.java
+++ src/main/java/com/jutools/FileUtil.java
@@ -3,8 +3,15 @@ package com.jutools;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.jutools.common.BoolType;
 
@@ -572,5 +579,79 @@ public class FileUtil {
 	 */
 	public static byte[] readNBytes(InputStream is, int n) throws Exception {
 		return readNBytes(is, n, 1024 * 1024);
+	}
+	
+	/**
+	 * stop 파일이 생성되거나 업데이트 될때까지 대기
+	 *
+	 * @param stopFile stop 파일
+	 * @param pollingPeriod 폴링 시간
+	 */
+	public static void waitForFileTouched(File stopFile, long pollingPeriod) throws Exception {
+
+		// 입력값 검증
+		if(stopFile == null) {
+			throw new IllegalArgumentException("stop file is null.");
+		}
+		
+		if(stopFile.getParentFile() == null) {
+			throw new IllegalArgumentException("stop file's parent is null.");
+		}
+
+		if(pollingPeriod < 100L) {
+			throw new IllegalArgumentException("polling period must be greater than 100: " + pollingPeriod);
+		}
+
+		// stop 파일명
+		String stopFileName = stopFile.getName();
+		
+		// 파일 생성 및 업데이트 이벤트 수신을 위한 Watch 서비스 생성 및 등록
+		Path parentPath = stopFile.toPath().getParent();
+		WatchService parentWatchService = parentPath.getFileSystem().newWatchService();
+		parentPath.register(parentWatchService
+			, StandardWatchEventKinds.ENTRY_CREATE
+			, StandardWatchEventKinds.ENTRY_MODIFY
+			);
+
+		while(true) {
+			
+			// WatchKey에 이벤트 들어올 때까지 대기
+			WatchKey watchKey = parentWatchService.poll(pollingPeriod, TimeUnit.MILLISECONDS);
+			if(watchKey == null) {
+				continue;
+			}
+
+			try {
+				
+				// 생성 및 업데이트 이벤트 목록 확인
+				List<WatchEvent<?>> eventList = watchKey.pollEvents();
+				for (WatchEvent<?> event : eventList) {
+			
+					WatchEvent.Kind<?> kind = event.kind();   // 이벤트 종류
+					Path eventFile = (Path) event.context();  // 이벤트가 발생한 파일
+
+					// 파일이 새로 생성되거나 업데이트(ex. touch 명령어)되면 대기 중지
+					if(kind == StandardWatchEventKinds.ENTRY_CREATE || kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+						if(stopFileName.equals(eventFile.toFile().getName()) == true) {
+							return;
+						}
+					}
+				}
+				
+			} finally {
+				// WatchKey 초기화 꼭 해줘야 함
+				watchKey.reset();
+			}
+		} // End of while
+	}
+	
+	/**
+	 * stop 파일이 생성되거나 업데이트 될때까지 대기
+	 * polling period = 1초
+	 *
+	 * @param stopFile stop 파일
+	 */
+	public static void waitForFileTouched(File stopFile) throws Exception {
+		waitForFileTouched(stopFile, 1000L);
 	}
 }
