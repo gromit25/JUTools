0f0bc1d06527777b2c1d43deae28ccc343132fca
jmsohn <gromit25@hanmail.net>
2025-08-03 18:36:19 +0900

    주석 추가

--- src/main/java/com/jutools/event/TimeoutEvent.java
+++ src/main/java/com/jutools/event/TimeoutEvent.java
@@ -3,16 +3,16 @@ package com.jutools.event;
 import lombok.Data;
 
 /**
- * 
+ * 타임아웃 발생 이벤트 클래스
  * 
  * @author jmsohn
  */
 @Data
 public class TimeoutEvent {
 
-	/** */
+	/** 타임 아웃 발생 시간 */
 	private long timestamp;
 
-	/** */
+	/** 최총 touch 된 시간 */
 	private long lastTouchedTimestamp;
 }
