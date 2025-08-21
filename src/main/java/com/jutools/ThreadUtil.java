01bedb75c6153df93ec0571e2106adda127b0595
jung mo <gromit25@hanmail.net>
2025-08-21 14:21:19 +0900

    Update ThreadUtil.java

--- src/main/java/com/jutools/ThreadUtil.java
+++ src/main/java/com/jutools/ThreadUtil.java
@@ -7,29 +7,33 @@ package com.jutools;
  */
 public class ThreadUtil {
 
-  public static void shutdown(ExecutorService execSvc) throws Exception {
+	public static void shutdown(ExecutorService execSvc) throws Exception {
     
-    execSvc.shutdown();
+		if(this.execSvc == null || this.execSvc.isShutdonw() == true) {
+			return;
+		}
     
-    try {
+		execSvc.shutdown();
+		
+		try {
       
-      // 모든 태스크가 완료될 때까지 지정된 시간(60초) 대기
-      if (execSvc.awaitTermination(60, TimeUnit.SECONDS) == false) {
-        
-        // 시간 초과 시 즉시 종료 시도
-        pool.shutdownNow(); 
-        
-        // 즉시 종료를 기다림
-        execSvc.awaitTermination(60, TimeUnit.SECONDS);
-      }
+			// 모든 태스크가 완료될 때까지 지정된 시간(60초) 대기
+			if (execSvc.awaitTermination(60, TimeUnit.SECONDS) == false) {
+
+				// 시간 초과 시 즉시 종료 시도
+				execSvc.shutdownNow(); 
+
+				// 즉시 종료를 기다림
+				execSvc.awaitTermination(60, TimeUnit.SECONDS);
+			}
       
-    } catch (InterruptedException ie) {
+		} catch (InterruptedException ie) {
       
-      // awaitTermination 중 인터럽트 발생 시 즉시 종료
-      pool.shutdownNow();
+			// awaitTermination 중 인터럽트 발생 시 즉시 종료
+			execSvc.shutdownNow();
       
-      // 현재 스레드의 인터럽트 상태를 유지
-      Thread.currentThread().interrupt();
-    }
-  }
+			// 현재 스레드의 인터럽트 상태를 유지
+			Thread.currentThread().interrupt();
+		}
+	}
 }
