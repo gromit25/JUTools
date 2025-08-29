ed4938ca3a7a906a791322321e9c6fc5c6308f06
jung mo <gromit25@hanmail.net>
2025-08-29 13:50:40 +0900

    Update MailConstant.java

--- src/main/java/com/jutools/mail/MailConstant.java
+++ src/main/java/com/jutools/mail/MailConstant.java
@@ -6,11 +6,22 @@ package com.jutools.mail;
  * @author jmsohn
  */
 public class MailConstant {
-	
+
+	/** 메일 서버 */
 	public static final String HOST = "mail.smtp.host";
+
+	/** 메일 서버 포트 */
 	public static final String PORT = "mail.smtp.port";
+
+	/** 인증 여부 */
 	public static final String AUTH = "mail.smtp.auth";
+
+	/** 메일 발송시 디버그 메시지 출력 여부 */
 	public static final String DEBUG = "mail.debug";
+
+	/** TLS 사용 여부 설정 */
 	public static final String TLS = "mail.smtp.starttls.enable";
+
+	/** SSL 사용 여부 설정 */
 	public static final String SSL = "mail.smtp.ssl.enable";
 }
