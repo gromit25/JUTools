a995da9aed2bcf54613997d8ff0b6d5718863a5e
jung mo <gromit25@hanmail.net>
2025-08-29 10:33:45 +0900

    Create MailConstant.java

---
+++ src/main/java/com/jutools/mail/MailConstant.java
@@ -0,0 +1,10 @@
+package com.jutools.mail;
+
+public class MailConstant {
+  public String HOST = "mail.smtp.host";
+  public String PORT = "mail.smtp.port";
+  public String AUTH = "mail.smtp.auth";
+  public String DEGUG = "mail.debug";
+  public String TTS = "mail.smtp.starttls.enable";
+  public String SSL = "mail.smtp.ssl.enable";
+}
