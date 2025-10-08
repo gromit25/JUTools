23baa0155e8aaa144bb63a7c91aed3ab7905729e
gromit25 <gromit25@hanmail.net>
2025-10-08 17:46:40 +0900

    워크플로우 커밋

---
+++ src/main/java/com/jutools/workflow/annotation/Init.java
@@ -0,0 +1,18 @@
+package com.jutools.workflow.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 초기화 메소드 어노테이션
+ * 
+ * @author jmsohn
+ */
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Init {
+
+}
---
+++ src/main/java/com/jutools/workflow/runner/LinkType.java
@@ -0,0 +1,15 @@
+package com.jutools.workflow.runner;
+
+/**
+ * 워크플로우 컴포넌트 종류
+ * 
+ * @author jmsohn
+ */
+public enum LinkType {
+	
+	/** 일반 컴포넌트 */
+	NORMAL,
+	
+	/** 앙림 핸들러 컴포넌트 */
+	NOTICE_HANDLER;
+}
