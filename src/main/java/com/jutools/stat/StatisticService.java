4969c33107ff85847e5ee2f495ed05835cd054a6
gromit25 <gromit25@hanmail.net>
2025-10-14 22:12:46 +0900

    오류 수정

--- src/main/java/com/jutools/stat/StatisticService.java
+++ src/main/java/com/jutools/stat/StatisticService.java
@@ -192,7 +192,7 @@ class CronBlock {
 		
 		// 데이터 로드 메소드 콜백
 		if(this.loader != null) {
-			this.loader.load(this.acquisitorCron.getCurrentBaseTime(), data, this.stat, isLast);
+			this.loader.load(this.acquisitorCron.getBaseTime(), data, this.stat, isLast);
 		}
 		
 		//
