e1a9f4077f047a4ef9ad92562f76f9c057cd28ff
jung mo <gromit25@hanmail.net>
2025-10-16 10:28:57 +0900

    Update SpringBeanUtil.java

--- src/main/java/com/jutools/SpringBeanUtil.java
+++ src/main/java/com/jutools/SpringBeanUtil.java
@@ -1,5 +1,12 @@
 package com.jutools;
 
+import java.lang.reflect.Constructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.stereotype.Component;
+
 @Component
 public class SpringBeanUtil {
   
@@ -7,19 +14,23 @@ public class SpringBeanUtil {
 	private GenericApplicationContext context;
 
 
-  /**
-   *
-   */
-	public StringBootUtil registerBean(String beanName, Object obj) throws Exception {
+	/**
+	 * 스프링 부트에 빈 수동 등록
+	 * 
+	 * @param beanName 등록할 빈 명칭
+	 * @param obj 등록할 빈 오브젝트
+	 * @return 현재 객체
+	 */
+	public SpringBeanUtil registerBean(String beanName, Object obj) throws Exception {
     
 		// 입력값 검증
-    if(StringUtil.isBlank(beanName) == true) {
-    	throw new IllegalArgumentException("'beanName' is null or blank.");
-    }
+		if(StringUtil.isBlank(beanName) == true) {
+			throw new IllegalArgumentException("'beanName' is null or blank.");
+		}
 
-    if(obj == null) {
-      throw new IllegalArgumentException("'obj' is null.");
-    }
+		if(obj == null) {
+			throw new IllegalArgumentException("'obj' is null.");
+		}
 
 		// 스프링 의존성 주입 수행
 		AutowireCapableBeanFactory factory = context.getAutowireCapableBeanFactory();
@@ -29,46 +40,52 @@ public class SpringBeanUtil {
 		// 컨텍스트에 등록
 		this.context.getBeanFactory().registerSingleton(beanName, obj);
 
-    // 등록된 빈 반환
-    return this;
+		// 등록된 빈 반환
+		return this;
 	}
 
-  /**
-   *
-   */
-	public StringBootUtil registerBean(String beanName, Class<?> clazz) throws Exception {
+	/**
+	 * 스프링 부트에 빈 수동 등록
+	 * 
+	 * @param beanName 등록할 빈 명칭
+	 * @param clazz 등록할 빈 클래스
+	 * @return 현재 객체
+	 */
+	public SpringBeanUtil registerBean(String beanName, Class<?> clazz) throws Exception {
 
-    // 입력값 검증
-    if(clazz == null) {
-      throw new IllegalArgumentException("'clazz' is null.");
-    }
-    
-    Constructor<?> constructor = clazz.getDeclaredConstructor();
-    if(clazz == null) {
-      throw new Exception(clazz + " has no default constructor.");
-    }
+		// 입력값 검증
+		if(clazz == null) {
+			throw new IllegalArgumentException("'clazz' is null.");
+		}
 
-    // 클래스의 객체 생성
-    Object obj = constructor.newInstance();
+		// 생성자 획득
+		Constructor<?> constructor = clazz.getDeclaredConstructor();
 
-    // 스프링부트에 등록
-    return this.registerBean(beanName, obj);
-  }
+		// 클래스의 객체 생성
+		Object obj = constructor.newInstance();
 
-    /**
-   *
-   */
-	public StringBootUtil registerBean(String beanName, String className) throws Exception {
+		// 스프링부트에 등록
+		return this.registerBean(beanName, obj);
+	}
 
-    // 입력값 검증
-    if(StringUtil.isBlank(className) == true) {
-      throw new IllegalArgumentException("'className' is null or blank.");
-    }
+	/**
+	 * 스프링 부트에 빈 수동 등록
+	 * 
+	 * @param beanName 등록할 빈 명칭
+	 * @param className 등록할 빈 클래스 문자열
+	 * @return 현재 객체
+	 */
+	public SpringBeanUtil registerBean(String beanName, String className) throws Exception {
 
-    // 클래스 로딩
+		// 입력값 검증
+		if(StringUtil.isBlank(className) == true) {
+			throw new IllegalArgumentException("'className' is null or blank.");
+		}
+
+		// 클래스 로딩
 		Class<?> clazz = Class.forName(className);
 
-    // 스프링부트에 등록
-    return this.registerBean(beanName, clazz);
-  }
+		// 스프링부트에 등록
+		return this.registerBean(beanName, clazz);
+	}
 }
