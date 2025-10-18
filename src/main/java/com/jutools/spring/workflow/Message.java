83fb44558cfb8506b3b8ed46c6280e1df45d34a9
gromit25 <gromit25@hanmail.net>
2025-10-18 20:59:07 +0900

    getBody 메소드 추가

--- src/main/java/com/jutools/spring/workflow/Message.java
+++ src/main/java/com/jutools/spring/workflow/Message.java
@@ -44,4 +44,15 @@ public class Message<T> {
 		this.setTopic(topic);
 		this.setBody(body);
 	}
+	
+	/**
+	 * 바디 데이터를 특정 타입으로 변환하여 반환 
+	 * 
+	 * @param <R> 반환 받을 타입
+	 * @param returnType 반환 받을 타입의 클래스
+	 * @return 바디 데이터
+	 */
+	public <R> R getBody(Class<R> returnType) {
+		return returnType.cast(this.body);
+	}
 }
