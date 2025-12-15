33e3fbf0e646566773bede6e943928d40e9ba6b4
gromit25 <gromit25@hanmail.net>
2025-12-15 23:07:25 +0900

    리팩토링 수행 중

--- src/main/java/com/jutools/CipherUtil.java
+++ src/main/java/com/jutools/CipherUtil.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -21,243 +22,53 @@ import com.jutools.common.OrderType;
 public class CipherUtil {
 	
 	/**
-	 * 문자열을 SHA 단방향 암호화 수행
+	 * SHA Utility 클래스
 	 * 
-	 * @param algorithm SHA 암호화 알고리즘 명
-	 * @param str 암호화할 문자열
-	 * @return 암호화된 문자열
+	 * @author jmsohn
 	 */
-	public static String encryptSHA(String algorithm, String str) throws Exception {
-		
-		// 입력값 검증
-		if(str == null) {
-			throw new Exception("str is null");
-		}
+	public static class SHAUtil {
 
-		// hash 생성 
-		MessageDigest digest = MessageDigest.getInstance(algorithm);
-		byte[] hash = digest.digest(str.getBytes());
-
-		// 문자열 변환 후 반환
-		return BytesUtil.bytesToStr(hash, OrderType.ASCEND);
-	}
+		/**
+		 * 문자열을 SHA 단방향 암호화 수행
+		 * 
+		 * @param str 암호화할 문자열
+		 * @param algorithm SHA 암호화 알고리즘 명
+		 * @return 암호화된 문자열
+		 */
+		public static String encryptSHA(String str, String algorithm) throws Exception {
+			
+			// 입력값 검증
+			if(str == null) {
+				throw new Exception("'str' is null.");
+			}
 	
-	/**
-	 * 문자열을 SHA-256 단방향 암호화 수행
-	 * 
-	 * @param str 암호화할 문자열
-	 * @return
-	 */
-	public static String encryptSHA256(String str) throws Exception {
-		return encryptSHA("SHA-256", str);
-	}
-
-	/**
-	 * 문자열을 SHA-512 단방향 암호화 수행
-	 * 
-	 * @param str 암호화할 문자열
-	 * @return 암호화된 문자열
-	 */
-	public static String encryptSHA512(String str) throws Exception {
-		return encryptSHA("SHA-512", str);
-	}
+			// hash 생성 
+			MessageDigest digest = MessageDigest.getInstance(algorithm);
+			byte[] hash = digest.digest(str.getBytes());
 	
-	// -----------------------------------
-
-	/**
-	 * AES-128 키 생성 반환 메소드
-	 * 
-	 * @return 생성된 AES 키
-	 */
-	public static String genAES128Key() throws Exception {
-		
-		// 키 생성
-		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-		keyGen.init(128); // 128비트 AES 키 생성
-		SecretKey key = keyGen.generateKey();
-
-		// 키를 문자열로 변환하여 반환
-		byte[] keyBytes = key.getEncoded();
-		return BytesUtil.bytesToStr(keyBytes, OrderType.ASCEND);
-	}
-	
-	/**
-	 * AES-256 키 생성 반환 메소드
-	 * 
-	 * @return 생성된 AES 키
-	 */
-	public static String genAES256Key() throws Exception {
-		
-		// 키 생성
-		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-		keyGen.init(256); // 256비트 AES 키 생성
-		SecretKey key = keyGen.generateKey();
-
-		// 키를 문자열로 변환하여 반환
-		byte[] keyBytes = key.getEncoded();
-		return BytesUtil.bytesToStr(keyBytes, OrderType.ASCEND);
-	}
-	
-	/**
-	 * 암호화 모듈 생성 메소드
-	 * 
-	 * @param opmode 생성 모드
-	 * @param key 암호화 키(32자리 문자열 - genAESKey 메소드를 통해 생성할 수 있음)
-	 * @return 암호화 모듈
-	 */
-	private static Cipher makeCipher(int opmode, String key) throws Exception {
-		
-		// 입력값 검증
-		if(StringUtil.isEmpty(key) == true) {
-			throw new Exception("key is empty");
+			// 문자열 변환 후 반환
+			return BytesUtil.bytesToStr(hash, OrderType.ASCEND);
 		}
 		
-		if(key.length() != 32 && key.length() != 64) {
-			throw new Exception("key str length must be 32 or 64");
+		/**
+		 * 문자열을 SHA-256 단방향 암호화 수행
+		 * 
+		 * @param str 암호화할 문자열
+		 * @return
+		 */
+		public static String encryptSHA256(String str) throws Exception {
+			return encryptSHA(str, "SHA-256");
 		}
-		
-		// 암호화 키 생성
-		byte[] keyBytes = new byte[key.length()/2];
-		
-		SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
-		
-		// 암호화 모듈 생성
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		cipher.init(opmode, secretKey);
-		
-		return cipher;
-	}
 	
-	/**
-	 * AES 암호화 메소드
-	 * 
-	 * @param key 암호화 키(32자리 문자열 - genAESKey 메소드를 통해 생성할 수 있음)
-	 * @param cs charset
-	 * @param str 암호화할 문자열
-	 * @return 암호화된 문자열
-	 */
-	public static String encryptAES(String key, Charset cs, String str) throws Exception {
-		
-		// 입력값 검증
-		if(cs == null) {
-			throw new Exception("charset is null");
+		/**
+		 * 문자열을 SHA-512 단방향 암호화 수행
+		 * 
+		 * @param str 암호화할 문자열
+		 * @return 암호화된 문자열
+		 */
+		public static String encryptSHA512(String str) throws Exception {
+			return encryptSHA(str, "SHA-512");
 		}
-		
-		// 암호화 모듈 생성
-		Cipher cipher = makeCipher(Cipher.ENCRYPT_MODE, key);
-		
-		// 암호화 수행
-		byte[] encryptedData = cipher.doFinal(str.getBytes(cs));
-		
-		// 인코딩하여 문자열로 만들어 반환
-		return BytesUtil.bytesToStr(encryptedData, OrderType.ASCEND);
-	}
-	
-	/**
-	 * AES 암호화 메소드
-	 * 
-	 * @param key 암호화 키(32자리 문자열 - genAESKey 메소드를 통해 생성할 수 있음)
-	 * @param str 암호화할 문자열
-	 * @return 암호화된 문자열
-	 */
-	public static String encryptAES(String key, String str) throws Exception {
-		return encryptAES(key, Charset.defaultCharset(), str);
-	}
-	
-	/**
-	 * AES 복호화 메소드
-	 * 
-	 * @param key 암호화 키(32자리 문자열 - genAESKey 메소드를 통해 생성할 수 있음)
-	 * @param cs charset
-	 * @param str 암호화된 문자열
-	 * @return 복호화된 문자열
-	 */
-	public static String decryptAES(String key, Charset cs, String str) throws Exception {
-		
-		// 입력값 검증
-		if(cs == null) {
-			throw new Exception("charset is null");
-		}
-		
-		// 암호화 모듈 생성
-		Cipher cipher = makeCipher(Cipher.DECRYPT_MODE, key);
-		
-		// 복호화 수행
-		byte[] decryptedData = cipher.doFinal(BytesUtil.strToBytes(str, OrderType.ASCEND));
-		
-		// 문자열로 만들어 반환
-		return new String(decryptedData, cs);
-	}
-	
-	/**
-	 * AES 복호화 메소드
-	 * 
-	 * @param key 암호화 키(32자리 문자열 - genAESKey 메소드를 통해 생성할 수 있음)
-	 * @param str 암호화된 문자열
-	 * @return 복호화된 문자열
-	 */
-	public static String decryptAES(String key, String str) throws Exception {
-		return decryptAES(key, Charset.defaultCharset(), str);
-	}
-	
-	// -----------------------------------
-	
-	/**
-	 * 
-	 * 
-	 * @param key
-	 * @return
-	 */
-	public static PublicKey loadPublicKey(byte[] key) throws Exception {
-		
-	}
-	
-	/**
-	 * 
-	 * 
-	 * @param keyFile
-	 * @return
-	 */
-	public static PublicKey loadPublicKey(File keyFile) throws Exception {
-		
-	}
-
-	/**
-	 * 
-	 * 
-	 * @param keyFileName
-	 * @return
-	 */
-	public static PublicKey loadPublicKey(String keyFileName) throws Exception {
-		
-	}
-
-	/**
-	 * 
-	 * 
-	 * @param key
-	 * @param str
-	 * @return
-	 */
-	public static String encryptPublicKey(PublicKey key, String str) throws Exception {
-		
-		// Cipher 객체 초기화 (알고리즘: RSA, 패딩: PKCS1Padding)
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-
-        // 평문 바이트를 암호화
-        return cipher.doFinal(str.getBytes("UTF-8"));
-	}
-
-	/**
-	 * 
-	 * 
-	 * @param key
-	 * @param str
-	 * @return
-	 */
-	public static String decryptPublicKey(PublicKey key, String str) throws Exception {
-		
 	}
 	
 	// -----------------------------------
@@ -265,52 +76,314 @@ public class CipherUtil {
 	/**
 	 * 
 	 * 
-	 * @param key
-	 * @return
+	 * @author jmsohn
 	 */
-	public static PrivateKey loadPrivateKey(byte[] key) throws Exception {
+	public static class AESUtil {
+	
+		/**
+		 * AES-128 키 생성 반환<br>
+		 * Base64로 인코딩됨
+		 * 
+		 * @return 생성된 AES 키
+		 */
+		public static String genAES128Key() throws Exception {
+			
+			// 키 생성
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+			keyGen.init(128); // 128비트 AES 키 생성
+			SecretKey key = keyGen.generateKey();
+	
+			// 키를 문자열로 변환하여 반환
+			byte[] keyBytes = key.getEncoded();
+			return Base64.getEncoder().encodeToString(keyBytes);
+		}
 		
-	}
-
+		/**
+		 * AES-256 키 생성 반환<br>
+		 * Base64로 인코딩됨
+		 * 
+		 * @return 생성된 AES 키
+		 */
+		public static String genAES256Key() throws Exception {
+			
+			// 키 생성
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+			keyGen.init(256); // 256비트 AES 키 생성
+			SecretKey key = keyGen.generateKey();
+	
+			// 키를 문자열로 변환하여 반환
+			byte[] keyBytes = key.getEncoded();
+			return Base64.getEncoder().encodeToString(keyBytes);
+		}
+		
+		/**
+		 * 암호화 모듈 생성 및 반환
+		 * 
+		 * @param opmode 생성 모드
+		 * @param key 암호화 키(base64 인코딩)
+		 * @return 암호화 모듈
+		 */
+		private static Cipher makeCipher(int opmode, String key) throws Exception {
+			
+			// 입력값 검증
+			if(StringUtil.isEmpty(key) == true) {
+				throw new Exception("key is empty");
+			}
+			
+			// 암호화 키 생성
+			byte[] keyBytes = new byte[key.length()/2];
+			
+			SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
+			
+			// 암호화 모듈 생성
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			cipher.init(opmode, secretKey);
+			
+			return cipher;
+		}
+		
+		/**
+		 * AES 암호화된 문자열 반환
+		 * 
+		 * @param str 암호화할 문자열
+		 * @param key 암호화 키(base64 인코딩)
+		 * @param cs charset
+		 * @return 암호화된 문자열(base64 인코딩)
+		 */
+		public static String encrypt(String str, String key, Charset cs) throws Exception {
+			
+			// 입력값 검증
+			if(cs == null) {
+				throw new Exception("charset is null");
+			}
+			
+			// 암호화 모듈 생성
+			Cipher cipher = makeCipher(Cipher.ENCRYPT_MODE, key);
+			
+			// 암호화 수행
+			byte[] encryptedData = cipher.doFinal(str.getBytes(cs));
+			
+			// 인코딩하여 문자열로 만들어 반환
+			return Base64.getEncoder().encodeToString(encryptedData);
+		}
+		
+		/**
+		 * AES 암호화된 문자열 반환
+		 * 
+		 * @param str 암호화할 문자열
+		 * @param key 암호화 키(base64 인코딩)
+		 * @return 암호화된 문자열(base64 인코딩)
+		 */
+		public static String encrypt(String str, String key) throws Exception {
+			return encrypt(str, key, Charset.defaultCharset());
+		}
+		
+		/**
+		 * AES 복호화된 문자열 반환
+		 * 
+		 * @param str 복호화할 문자열(base64 인코딩)
+		 * @param key 복호화 키(base64 인코딩)
+		 * @param cs charset
+		 * @return 복호화된 문자열
+		 */
+		public static String decrypt(String str, String key, Charset cs) throws Exception {
+			
+			// 입력값 검증
+			if(cs == null) {
+				throw new Exception("charset is null");
+			}
+			
+			// 암호화 모듈 생성
+			Cipher cipher = makeCipher(Cipher.DECRYPT_MODE, key);
+			
+			// 복호화 수행
+			byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(str));
+			
+			// 문자열로 만들어 반환
+			return new String(decryptedData, cs);
+		}
+		
+		/**
+		 * AES 복호화된 문자열 반환
+		 * 
+		 * @param str 복호화할 문자열(base64 인코딩)
+		 * @param key 복호화 키(base64 인코딩)
+		 * @return 복호화된 문자열
+		 */
+		public static String decryptAES(String str, String key) throws Exception {
+			return decrypt(str, key, Charset.defaultCharset());
+		}
+	} // End of AESUtil
+	
+	// -----------------------------------
+	
+	/**
+	 * Public Key 유틸리티 클래스
+	 * 
+	 * @author jmsohn
+	 */
+	public static class PublicKeyUtil { 
+	
+		/**
+		 * 
+		 * 
+		 * @param key
+		 * @return
+		 */
+		public static PublicKey load(byte[] key) throws Exception {
+			
+		}
+		
+		/**
+		 * 
+		 * 
+		 * @param keyFile
+		 * @return
+		 */
+		public static PublicKey load(File keyFile) throws Exception {
+			
+			if(keyFile == null) {
+				throw new IllegalArgumentException("'keyFile' is null.");
+			}
+			
+			if(keyFile.canRead() == false) {
+				throw new IllegalArgumentException("can't read key file: " + keyFile.getAbsolutePath());
+			}
+			
+			return load(FileUtil.readAllBytes(keyFile));
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param base64Key
+		 * @return
+		 */
+		public static PublicKey load(String base64Key) throws Exception {
+			
+			if(StringUtil.isBlank(base64Key) == true) {
+				throw new IllegalArgumentException("'base64Key' is null or blank.");
+			}
+			
+			return load(Base64.getDecoder().decode(base64Key));
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param str 
+		 * @param key
+		 * @return
+		 */
+		public static String encrypt(String str, PublicKey key) throws Exception {
+			
+			// 입력값 검증
+			if(key == null) {
+				throw new IllegalArgumentException("'key' is null.");
+			}
+			
+			// 입력 문자열이 null 이거나 blank 이면 빈 문자열 반환
+			if(StringUtil.isBlank(str) == true) {
+				return "";
+			}
+			
+			// Cipher 객체 초기화 (알고리즘: RSA, 패딩: PKCS1Padding)
+	        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+	        cipher.init(Cipher.ENCRYPT_MODE, key);
+	
+	        // 평문 바이트를 암호화
+	        return Base64.getEncoder().encodeToString(
+	        	cipher.doFinal(str.getBytes("UTF-8"))
+	        );
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param str
+		 * @param key
+		 * @return
+		 */
+		public static String decrypt(String str, PublicKey key) throws Exception {
+			
+		}
+		
+	} // End of PublicKeyUtil
+	
+	// -----------------------------------
+	
 	/**
 	 * 
 	 * 
-	 * @param keyFile
-	 * @return
+	 * @author jmsohn
 	 */
-	public static PrivateKey loadPrivateKey(File keyFile) throws Exception {
-		
-	}
+	public static class PrivateKeyUtil {
 
-	/**
-	 * 
-	 * 
-	 * @param keyFileName
-	 * @return
-	 */
-	public static PrivateKey loadPrivateKey(String keyFileName) throws Exception {
+		/**
+		 * 
+		 * 
+		 * @param key
+		 * @return
+		 */
+		public static PrivateKey load(byte[] key) throws Exception {
+			
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param keyFile
+		 * @return
+		 */
+		public static PrivateKey load(File keyFile) throws Exception {
+			
+			if(keyFile == null) {
+				throw new IllegalArgumentException("'keyFile' is null.");
+			}
+			
+			if(keyFile.canRead() == false) {
+				throw new IllegalArgumentException("can't read key file: " + keyFile.getAbsolutePath());
+			}
+			
+			return load(FileUtil.readAllBytes(keyFile));
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param base64Key
+		 * @return
+		 */
+		public static PrivateKey load(String base64Key) throws Exception {
+			
+			if(StringUtil.isBlank(base64Key) == true) {
+				throw new IllegalArgumentException("'base64Key' is null or blank.");
+			}
+			
+			return load(Base64.getDecoder().decode(base64Key));
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param key
+		 * @param str
+		 * @return
+		 */
+		public static String encrypt(PrivateKey key, String str) throws Exception {
+			
+		}
+	
+		/**
+		 * 
+		 * 
+		 * @param key
+		 * @param str
+		 * @return
+		 */
+		public static String decrypt(PrivateKey key, String str) throws Exception {
+			
+		}
 		
-	}
-
-	/**
-	 * 
-	 * 
-	 * @param key
-	 * @param str
-	 * @return
-	 */
-	public static String encryptPrivateKey(PrivateKey key, String str) throws Exception {
-		
-	}
-
-	/**
-	 * 
-	 * 
-	 * @param key
-	 * @param str
-	 * @return
-	 */
-	public static String decryptPrivateKey(PrivateKey key, String str) throws Exception {
-		
-	}
+	} // End of PrivateKeyUtil 
 }
