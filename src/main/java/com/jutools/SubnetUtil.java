177259c792430075debd4a31c3569c4351fb2adc
jung mo <gromit25@hanmail.net>
2026-03-17 17:46:14 +0900

    Update SubnetUtil.java

--- src/main/java/com/jutools/SubnetUtil.java
+++ src/main/java/com/jutools/SubnetUtil.java
@@ -3,16 +3,25 @@ package com.jutools;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *
+ *
+ * @author jmsohn
+ */
 public class SubnetUtil {
 
-	
+
+	/** */
 	private static String CIDR_PATTERN = "(?<ip>[0-9]{1,3}(\\.[0-9]{1,3}){3})\\/(?<prefixLength>[0-9]{1,2})";
-	
+
+	/** */
 	private static Pattern cidrP = Pattern.compile(CIDR_PATTERN);
 	
-	
+
+	/** */
 	private long cidr;
-	
+
+	/** */
 	private long mask;
 	
 	
@@ -21,7 +30,7 @@ public class SubnetUtil {
 	 * 
 	 * @param cidrStr
 	 */
-	public SubnetUtil(String cidrStr) throws Exception {
+	private SubnetUtil(String cidrStr) throws Exception {
 		
 		// 입력값 검증 및 패턴 매칭
 		Matcher cidrM = cidrP.matcher(cidrStr);
@@ -32,52 +41,61 @@ public class SubnetUtil {
 		
 		// IP 설정
 		this.cidr = ipToLong(cidrM.group("ip"));
-		
-        // CIDR 접두사 길이에 따른 서브넷 마스크 생성
-        // 예: /16 -> 11111111 11111111 00000000 00000000
+
+		// CIDR 접두사 길이에 따른 서브넷 마스크 생성
+		// 예: /16 -> 11111111 11111111 00000000 00000000
 		int prefixLength = Integer.parseInt(cidrM.group("prefixLength"));
 		this.mask = (prefixLength == 0) ? 0 : 0xFFFFFFFF << (32 - prefixLength);
 	}
+
+	/**
+	 *
+	 *
+	 * @param cidrStr
+	 */
+	public static SubnetUtil create(cidrStr) throws Exception {
+		return new SubnetUtil(cidrStr);
+	}
 	
 	/**
-     * IP 문자열을 32비트 정수(int)로 변환
-     * 
-     * @param ipStr
-     */
-    private static long ipToLong(String ipStr) throws Exception {
-    	
-        String[] octets = ipStr.split("\\.");
+	 * IP 문자열을 32비트 정수(int)로 변환
+	 * 
+	 * @param ipStr
+	 */
+	private static long ipToLong(String ipStr) throws Exception {
+
+		String[] octets = ipStr.split("\\.");
         
-        long result = 0;
-        for (int i = 0; i < 4; i++) {
-        	
-        	//
-            int octet = Integer.parseInt(octets[i]);
-            
-            // 각 마디를 8비트씩 왼쪽으로 밀어서 합침
-            result |= (octet << (24 - (i * 8)));
-        }
-        
-        return result;
-    }
+		long ip = 0;
+		for (int index = 0; i < 4; i++) {
+
+			//
+			int octet = Integer.parseInt(octets[i]);
+
+			// 각 마디를 8비트씩 왼쪽으로 밀어서 합침
+			ip |= (octet << (24 - (i * 8)));
+		}
+
+		return ip;
+	}
 	
 	/**
-     * 주어진 ip가 범위에 포함되는지 확인
-     * 
-     * @param ipStr
-     */
-    public boolean isInRange(String ipStr) {
-    	
-        try {
+	 * 주어진 ip가 범위에 포함되는지 확인
+	 * 
+	 * @param ipStr
+	 */
+	public boolean isInRange(String ipStr) {
 
-        	// 
-            long ip = ipToLong(ipStr);
+		try {
 
-            // IP의 네트워크 파트(앞부분)가 일치하는지 비트 AND 연산으로 비교
-            return (this.cidr & this.mask) == (ip & this.mask);
-            
-        } catch (Exception e) {
-            return false;
-        }
-    }
+			// 
+			long ip = ipToLong(ipStr);
+
+			// IP의 네트워크 파트(앞부분)가 일치하는지 비트 AND 연산으로 비교
+			return (this.cidr & this.mask) == (ip & this.mask);
+
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
