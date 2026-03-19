5fa7eb6d6db6876412e475b2128b02aa2ac2a7d7
jung mo <gromit25@hanmail.net>
2026-03-19 14:38:32 +0900

    Update SubnetUtil.java

--- src/main/java/com/jutools/SubnetUtil.java
+++ src/main/java/com/jutools/SubnetUtil.java
@@ -11,7 +11,10 @@ import java.util.regex.Pattern;
 public class SubnetUtil {
 
 
-	/** CIDR(Classless Inter-Domain Routing) 패턴 문자열 */
+	/**
+	 * CIDR(Classless Inter-Domain Routing) 패턴 문자열<br>
+	 * ex) 10.10.0.0/16
+	 */
 	private static String CIDR_PATTERN = "(?<ip>[0-9]{1,3}(\\.[0-9]{1,3}){3})\\/(?<prefixLength>[0-9]{1,2})";
 
 	/** CIDR 패턴 객체 */
